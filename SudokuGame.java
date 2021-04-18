5de0e8222cf5dcbdf52f3fc494ce141e815b3eb8
Josh Martin <74119895+Uniexus@users.noreply.github.com>
2021-04-18 16:04:44 -0400

    Add files via upload

---
+++ SudokuGame.java
@@ -0,0 +1,34 @@
+public class SudokuGame {
+    public static void main(String[] args){
+        int[][] exampleGrid = {{5, 3, 0, 0, 7, 0, 0, 0, 0}, 
+                               {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                               {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                               {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                               {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                               {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                               {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                               {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                               {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+        
+        
+        Sudoku sudoku = new Sudoku(exampleGrid);
+        sudoku.printGrid();
+        System.out.println();
+        
+        System.out.print("Row 2: ");
+        sudoku.printRow(2);
+        System.out.println();
+        
+        System.out.print("Column 9: ");
+        sudoku.printCol(9);
+        System.out.println();
+        
+        System.out.print("Box 5: ");
+        sudoku.printBox(5);
+        System.out.println();
+
+        
+
+    }
+    
+}
